0a9d2dc23b3f281380ba0d3670edf65b87ce4464
sweta <swetaarawal972@gmail.com>
2025-02-22 12:15:45 +0530

    [sweta Agrawal]Add. uc4_PutRequest  added

--- src/main/java/com/example/SpringMessagingApplication/controller/MessagingApplication.java
+++ src/main/java/com/example/SpringMessagingApplication/controller/MessagingApplication.java
@@ -20,6 +20,10 @@ public class MessagingApplication {
     public String greetUser(@RequestBody UserDTO user) {
         return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
     }
+    @PutMapping("/hello/put/{firstName}")
+    public String greetUserPut(@PathVariable String firstName, @RequestParam String lastName) {
+        return "Hello " + firstName + " " + lastName + " from BridgeLabz";
+}
 
 
 }
